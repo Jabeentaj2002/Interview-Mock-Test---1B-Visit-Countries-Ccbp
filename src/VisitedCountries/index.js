398c4a4f6dca74be9deefb0cb33ded6c180fcdca
jabeen <jabeentaj1532@gmail.com>
2024-11-21 08:17:04 +0000

    first commit

---
+++ src/VisitedCountries/index.js
@@ -0,0 +1,28 @@
+import {
+  VisitedCountryContainer,
+  Flag,
+  NameContainer,
+  CardCountryName,
+  RemoveBtn,
+} from './styledComponents'
+
+const VisitedCountries = props => {
+  const {visitedCountryDetails, onRemoveCountry} = props
+  const {id, name, imageUrl} = visitedCountryDetails
+
+  const onRemoveBtnClicked = () => {
+    onRemoveCountry(id)
+  }
+
+  return (
+    <VisitedCountryContainer>
+      <Flag src={imageUrl} alt="thumbnail"></Flag>
+      <NameContainer>
+        <CardCountryName>{name}</CardCountryName>
+        <RemoveBtn onClick={onRemoveBtnClicked}>Remove</RemoveBtn>
+      </NameContainer>
+    </VisitedCountryContainer>
+  )
+}
+
+export default VisitedCountries
